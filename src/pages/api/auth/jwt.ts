7e36e0c4d01fe9d75390fcffbf2f4940d1119b0a
T-taku <t-takuscratch@outlook.jp>
2023-06-04 23:45:59 +0900

    [Fix] 不要パッケージの削除

--- src/pages/api/auth/jwt.ts
+++ src/pages/api/auth/jwt.ts
@@ -1,6 +1,5 @@
 // This is an example of how to read a JSON Web Token from an API route
 import { getToken } from "next-auth/jwt"
-import jwt from "jsonwebtoken";
 import type { NextApiRequest, NextApiResponse } from "next"
 
 const secret = process.env.NEXTAUTH_SECRET;
