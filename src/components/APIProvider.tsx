91e2dba13aac50327bf089d1bac911ae752ee10b
suyo_haru <suyo_haru@outlook.jp>
2024-06-01 16:34:40 +0900

    [Fix] NEXT_PUBLIC_USE_LOCALを見るように修正

--- src/components/APIProvider.tsx
+++ src/components/APIProvider.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import { createContext } from "react";
 
 export const APIContext = createContext<Promise<RegiAPI> | undefined>(undefined);
+const local = process.env.NEXT_PUBLIC_USE_LOCAL == "true";
 
 export function APIProvider({ children }: { children: React.ReactNode }) {
   const api = fetch('/api/auth/jwt')
-    .then((res) => res.text()).then((token) => new RegiAPI(token));
+    .then((res) => res.text()).then((token) => new RegiAPI(token, local));
   return <APIContext.Provider value={api}>
     {children}
   </APIContext.Provider>
