65006c241b24a98b30f802c203678516c2407dc8
suyo_haru <suyo_haru@outlook.jp>
2024-06-04 01:34:10 +0900

    [Add] バックアップサーバーを追加

--- src/components/APIProvider.tsx
+++ src/components/APIProvider.tsx
@@ -4,13 +4,14 @@ import { createContext } from "react";
 
 export const APIContext = createContext<RegiAPI | undefined>(undefined);
 const local = process.env.NEXT_PUBLIC_USE_LOCAL == "true";
+const backup = process.env.NEXT_PUBLIC_USE_BACKUP == "true";
 
 export function APIProvider({ children }: { children: React.ReactNode }) {
   const [api, setAPI] = useState<RegiAPI | undefined>(undefined);
   useEffect(() => {
     fetch('/api/auth/jwt')
       .then(res => res.text())
-      .then(token => setAPI(new RegiAPI(token, local)))
+      .then(token => setAPI(new RegiAPI(token, local, backup)))
       .catch(() => setAPI(new RegiAPI()));
   }, [])
   return <APIContext.Provider value={api}>
